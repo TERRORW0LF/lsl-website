0e8fed0c5f34d1bcaf6a0d7839a4342611dcf5b9
Finn Brandt <finn2003minicooper@gmail.com>
2026-09-09 22:22:31 +0200

    wip: map management

--- server/src/admin.rs
+++ server/src/admin.rs
@@ -7,7 +7,45 @@ use types::{
 
 // TODO: Move rank and title updates into rating update trigger tied to rank table
 #[server(RecalculateRankings, prefix="/api", endpoint="ranking/recalculate", input=GetUrl)]
-pub async fn recalculate_ranks() -> Result<(), ApiError> {
+pub async fn recalculate_ranks(layout: Option<String>, category: Option<String>) -> Result<(), ApiError> {
+    use crate::auth::ssr::{auth, pool};
+
+    let user = auth()?.current_user.ok_or(ApiError::Unauthenticated)?;
+    user.has(&Permissions::ManageRuns).ok_or(ApiError::Unauthorized)?;
+    if layout.is_some() && category.is_none() || (layout.is_none() && category.is_some()) {
+        return Err(ApiError::InvalidInput);
+    }
+    let pool = pool()?;
+
+    let _ = sqlx::query(
+        r#"WITH ran AS (SELECT (percentage ^ (1 / (1.3 + percentage))) AS p, user_id AS u 
+				FROM rank
+				WHERE patch = '2.13' AND layout IS NOT DISTINCT FROM $1 
+					AND category IS NOT DISTINCT FROM $2),
+		po AS (SELECT AVG(points) as p, user_id as u 
+			FROM run r 
+			JOIN section s ON section_id = s.id
+			WHERE s.patch = '2.13' AND layout = $1
+				AND category = $2 AND submittable = true
+			GROUP BY user_id)
+		UPDATE rank r
+		SET points = (SELECT p FROM po WHERE u = r.user_id),
+		rating = (-10000 * (SELECT p FROM ran WHERE u = r.user_id) * (
+			(EXP((-(SELECT p FROM po WHERE u = r.user_id) + 1) ^ 
+				(1 / (12 - 10.7 * (-EXP((-(SELECT p FROM po WHERE u = r.user_id) + 1) ^ 
+		            (1 / (1.9 * (3 - 2 * (SELECT p FROM po WHERE u = r.user_id)) 
+                        * (2 - (SELECT p FROM ran WHERE u = r.user_id))))) 
+				+ EXP(1.0)) / (EXP(1.0) - 1)))) 
+			- EXP(1.0)) / (EXP(1.0) - 1))),
+		updated_at = NEW.created_at
+		WHERE patch = '2.13' AND layout IS NOT DINSTINCT FROM $1 AND category IS NOT DISTINCT FROM $2;"#,
+    )
+    .bind(layout)
+    .bind(category)
+    .execute(&pool)
+    .await
+    .map_err(|_| ApiError::ServerError("Database update failed".into()))?;
+
     Ok(())
 }
 
@@ -34,7 +72,7 @@ pub async fn recalculate_runs(layout: String, category: String, map: String) -> 
     .bind(map)
     .execute(&pool)
     .await
-    .map_err(|_| ApiError::ServerError("Database update failed".into()));
+    .map_err(|_| ApiError::ServerError("Database update failed".into()))?;
 
     Ok(())
 }
@@ -64,7 +102,7 @@ pub async fn add_section(
     .bind(submittable)
     .execute(&pool)
     .await
-    .map_err(|_| ApiError::ServerError("Database insert failed".into()));
+    .map_err(|_| ApiError::ServerError("Database insert failed".into()))?;
 
     Ok(())
 }
@@ -94,7 +132,7 @@ pub async fn update_section(id: i32, submittable: Option<bool>) -> Result<(), Ap
         .persistent(false)
         .execute(&pool)
         .await
-        .map_err(|_| ApiError::ServerError("Database update failed".into()));
+        .map_err(|_| ApiError::ServerError("Database update failed".into()))?;
 
     Ok(())
 }
